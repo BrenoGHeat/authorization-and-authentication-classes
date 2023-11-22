8a21d399ae5229d2fa67756f08f37adaf3cc6aca
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2023-11-22 10:48:31 -0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -9,9 +9,28 @@ app.use(helmet());
 
 app.use(json());
 
+app.get("/auth", (req, res) => {
+    try {
+        const token = req.headers.authorization;
+
+        const secret = process.env.JWT_SECRET;
+
+        if(token && secret){
+            jwt.verify(token, secret);
+
+            const payload = jwt.decode(token);
+
+            return res.status(200).json(payload);
+        }
+        
+    } catch (error) {
+        return res.status(403).json(error);
+    }
+})
+
 app.post("/login", (req, res) => {
    if (process.env.JWT_SECRET) {
-      const token = jwt.sign({ id: 1 }, process.env.JWT_SECRET);
+      const token = jwt.sign({ id: 1 }, process.env.JWT_SECRET, { expiresIn: "24h"});
 
       return res.status(200).json({ accessToken: token });
    }
